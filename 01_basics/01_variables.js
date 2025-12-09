e5c04bc19dfaab5a16930bf9924afa02a8e80bb5
RajatSingh02160 <rajatsingh2710.24@kgpian.iitkgp.ac.in>
2025-12-09 14:05:25 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 45216
+let accountEmail = "raja@34"
+var accountState = "1234"
+accountCity = "Jaipur"
+let accountPassword;
+
+
+// accountID = 12344 //  NOT ALLOWED
+/*
+Prefer noy to use var
+because of issue of block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountState,accountCity,accountPassword])
