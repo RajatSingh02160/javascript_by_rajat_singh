87e323f4bbdd1460bcace0994cf1e0ce99c86f81
RajatSingh02160 <rajatsingh2710.24@kgpian.iitkgp.ac.in>
2025-12-10 12:31:16 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let name = null 
+let age = 21
+let isNumber = true
+
+console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof isNumber)
+
+// undefined => undefined
+// null => object
