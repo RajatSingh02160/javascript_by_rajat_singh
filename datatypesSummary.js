d469938870980cd0f761fcb638344cba5fc0c424
RajatSingh02160 <rajatsingh2710.24@kgpian.iitkgp.ac.in>
2025-12-10 16:02:06 +0000

    container config done

---
+++ datatypesSummary.js
@@ -0,0 +1,29 @@
+// Primitive 
+// 7 types => String , Number , Boolean, null , undefined , Symbol , BigInt
+
+const score = 100
+const scoreNumber = 100.3
+const isLogged = false
+const temp = null
+let userEmail;
+const id = Symbol('123')
+
+
+
+
+
+
+
+
+// Reference ( Non - primitive)  
+// Array , Objects , Functions 
+
+const hero = ["rajat","singh","kaviya"];
+let myObject = {
+    name: "rajat",
+    age: 22,
+
+}
+const myFunction = function(){
+    console.log("helloWorld")
+}
